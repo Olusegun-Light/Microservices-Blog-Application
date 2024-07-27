ff8c0cbfbe4a85adc1117c212a14135808435b93
Olusegun-Light <lightolusegun1@gmail.com>
2024-07-27 22:03:26 +0100

    communication between services

--- comments/index.js
+++ comments/index.js
@@ -24,7 +24,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post("http://127.0.0.1:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -53,7 +53,7 @@ app.post("/events", async (req, res) => {
 
     comment.status = status;
 
-    await axios.post("http://127.0.0.1:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
